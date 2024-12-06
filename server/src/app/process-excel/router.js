b55e013b3010846df9bb8e82ca88e85b5d8b0e47
lovhurt <alicandagidir@hotmail.com>
2024-12-06 10:12:50 +0300

    new type

--- server/src/app/process-excel/router.js
+++ server/src/app/process-excel/router.js
@@ -1,86 +1,95 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-const XLSX = require('xlsx');
-const path = require('path');
-const fs = require('fs');
+const XLSX = require("xlsx");
+const path = require("path");
+const fs = require("fs");
 
-router.post('/process-excel', upload.single('file'), (req, res) => {
+router.post("/process-excel", upload.single("file"), (req, res) => {
   try {
-    console.log('Dosya işleme başlıyor...');
+    console.log("Dosya işleme başlıyor...");
 
     const file = req.file;
 
-    if (!file) return res.status(400).json({ error: 'Dosya yüklenmedi' });
+    if (!file) return res.status(400).json({ error: "Dosya yüklenmedi" });
 
-    // Dosya buffer'ını okuyun
     const buffer = file.buffer;
 
     // Excel dosyasını okuyun
-    const workbook = XLSX.read(buffer, { type: 'buffer' });
-    console.log('Çalışma kitabı işlendi.');
+    const workbook = XLSX.read(buffer, { type: "buffer" });
+    console.log("Çalışma kitabı işlendi.");
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const data = XLSX.utils.sheet_to_json(sheet);
 
     if (!data || data.length === 0)
-      return res.status(400).json({ error: 'Excel dosyası boş veya geçersiz formatta' });
-    console.log('Veri çıkarıldı:', data);
+      return res
+        .status(400)
+        .json({ error: "Excel dosyası boş veya geçersiz formatta" });
+    console.log(
+      "Veri çıkarıldı:"
+      // data
+    );
 
     // Verileri işleyin
     const processedData = [];
     const baseCodeCounts = {};
+    const categoriesData = {};
 
     data.forEach((row) => {
-      const originalProductCode = row["Ürün Kodu"];
+      const salePrice = row["Piyasa Satış Fiyatı (KDV Dahil)"];
+      const trendyolPrice = row["Trendyol'da Satılacak Fiyat (KDV Dahil)"];
 
-      // Base code'u çıkar
+      const originalProductCode = row["Model Kodu"];
       const baseProductCode = extractBaseProductCode(originalProductCode);
 
-       // Base code için sayaç oluştur veya artır
-       if (!baseCodeCounts[baseProductCode]) {
+      // Base code için sayaç oluştur veya artır
+      if (!baseCodeCounts[baseProductCode]) {
         baseCodeCounts[baseProductCode] = 1;
       } else {
         baseCodeCounts[baseProductCode] += 1;
       }
 
       const variantNumber = baseCodeCounts[baseProductCode];
-
       const isFirstVariant = variantNumber === 1;
-
-      // Ürün Kodu'nun sonunda varsa ekstra tireyi kaldır
       const variantProductCode = `${baseProductCode}-${variantNumber}`;
+      const category = row["Kategori İsmi"];
+
+      const stockAmount = (salePrice == 0 || trendyolPrice == 0) ? "0" : "5";
+
 
       // Veri nesnesini oluşturun
       const dataObject = {
         "Kategori No": row["Kategori No"],
-        "Kategori Açıklama": row["Kategori Açıklama"],
+        "Kategori İsmi": row["Kategori İsmi"],
         "Ürün Adı": row["Ürün Adı"],
         "Ürün Kodu": baseProductCode, // Tek ürün kodu
-        "Marka": row["Marka"],
+        Marka: row["Marka"],
         "Varyant - Ürün Kodu": variantProductCode,
-        "Varyant - Renk": row["Varyant - Renk"],
-        "Ürün Stok Miktarı": "5",
-        "Liste Fiyatı (Kdv Dahil)": row["Liste Fiyatı (Kdv Dahil)"],
-        "Goturc İndirimli Satış Fiyatı (Kdv Dahil)": row["Goturc İndirimli Satış Fiyatı (Kdv Dahil)"] || "",
-        "Para Birimi": row["Para birimi"],
-        "Görsel1": row["Görsel1"] || "",
-        "Görsel2": row["Görsel2"] || "",
-        "Görsel3": row["Görsel3"] || "",
-        "Görsel4": row["Görsel4"] || "",
-        "Görsel5": row["Görsel5"] || "",
-        "Görsel6": row["Görsel6"] || "",
-        "Görsel7": row["Görsel7"] || "",
-        "Görsel8": row["Görsel8"] || "",
-        "Görsel9": row["Görsel9"] || "",
-        "Ürün Açıklama": row["Ürün Açıklama"],
-        "Beden": "",
+        "Varyant - Renk": row["Ürün Rengi"],
+        "Varyant - Beden": row["Beden"],
+        "Ürün Stok Miktarı": stockAmount,
+        "Liste Fiyatı (Kdv Dahil)": row["Piyasa Satış Fiyatı (KDV Dahil)"],
+        "Goturc İndirimli Satış Fiyatı (Kdv Dahil)":
+          row["Trendyol'da Satılacak Fiyat (KDV Dahil)"] || "",
+        "Para Birimi": row["Para birimi"] || "TL",
+        Görsel1: row["Görsel 1"] || "",
+        Görsel2: row["Görsel 2"] || "",
+        Görsel3: row["Görsel 3"] || "",
+        Görsel4: row["Görsel 4"] || "",
+        Görsel5: row["Görsel 5"] || "",
+        Görsel6: row["Görsel 6"] || "",
+        Görsel7: row["Görsel 7"] || "",
+        Görsel8: row["Görsel 8"] || "",
+        Görsel9: row["Görsel 9"] || "",
+        "Ürün Açıklama": row["Ürün Açıklaması"] || row["Ürün Adı"],
+        Beden: "",
         "Garanti Süresi": "",
-        "Materyal": "",
+        Materyal: "",
         "Uyumlu Marka": "",
-        "Hazırlık Süresi": row["Hazırlık Süresi (Gün)"] || 2,
-        "Kargo Şablonu": row["Kargo Şablonu"] || "Piyasa Sepeti",
+        "Hazırlık Süresi": row["Sevkiyat Süresi"] || 2,
+        "Kargo Şablonu": row["Kargo Şablonu"] || "",
       };
 
       // // Eğer bu ürünün ilk varyantıysa, ek alanları ekleyin
@@ -91,59 +100,67 @@ router.post('/process-excel', upload.single('file'), (req, res) => {
       //   dataObject["Tema / Stil"] = row["Tema / Stil"] || "";
       // }
 
-      processedData.push(dataObject);
+      // processedData.push(dataObject);
 
-      console.log(`Orijinal Ürün Kodu: ${originalProductCode}, Base Product Code: ${baseProductCode}, Varyant No: ${variantNumber}`);
-
+      if (!categoriesData[category]) {
+        categoriesData[category] = [];
+      }
+      categoriesData[category].push(dataObject);
+      // console.log(`Orijinal Ürün Kodu: ${originalProductCode}, Base Product Code: ${baseProductCode}, Varyant No: ${variantNumber}`);
     });
 
-    // Base product code'u çıkarmak için fonksiyon
     function extractBaseProductCode(productCode) {
       // Güncellenmiş regex ile boyut bilgisini eşleştir ve kaldır
       const regex = /^(.*?)[0-9]+x[0-9]+(.*?)-?$/;
       const match = productCode.match(regex);
       if (match) {
-        return `${match[1]}${match[2]}`.replace(/-$/, '');
+        return `${match[1]}${match[2]}`.replace(/-$/, "");
       } else {
         // Eşleşme yoksa orijinal ürün kodundan sonundaki '-' işaretini kaldır
-        return productCode.replace(/-$/, '');
+        return productCode.replace(/-$/, "");
       }
     }
 
-    // Çıktı dizinini ve yolunu belirleyin
-    const outputDir = path.join(__dirname, '../../excels');
-    const outputPath = path.join(outputDir, 'deneme14.xlsx');
+    const outputDir = path.join(__dirname, "../../excels");
 
-    // Çıktı dizini yoksa oluşturun
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir, { recursive: true });
       console.log(`Çıktı dizini oluşturuldu: ${outputDir}`);
     }
 
-    // Yeni Excel dosyasını oluşturun
-    const outputData = processedData; // İşlenen veriler
-    const outputSheet = XLSX.utils.json_to_sheet(outputData);
+    const outputPaths = [];
 
-    // Başlıkları eklemek için
-    const headers = Object.keys(outputData[0]);
-    XLSX.utils.sheet_add_aoa(outputSheet, [headers], { origin: 'A1' });
+    for (const category in categoriesData) {
+      const outputData = categoriesData[category];
+      const outputSheet = XLSX.utils.json_to_sheet(outputData);
 
-    const outputWorkbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(outputWorkbook, outputSheet, 'Sonuç');
+      const headers = Object.keys(outputData[0]);
+      XLSX.utils.sheet_add_aoa(outputSheet, [headers], { origin: "A1" });
 
-    // Dosyayı yazın
-    const wbout = XLSX.write(outputWorkbook, { bookType: 'xlsx', type: 'buffer' });
-    fs.writeFileSync(outputPath, wbout);
-    console.log('Dosya başarıyla yazıldı:', outputPath);
+      const outputWorkbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(outputWorkbook, outputSheet, "Sonuç");
 
-    // İstemciye başarılı mesajı gönderin
+      const outputPath = path.join(
+        outputDir,
+        `XXXXXXXXXXXXXXXXXXXXXXXXXXXX${category}_YeniUrun.xlsx`
+      );
+      const wbout = XLSX.write(outputWorkbook, {
+        bookType: "xlsx",
+        type: "buffer",
+      });
+      fs.writeFileSync(outputPath, wbout);
+
+      outputPaths.push(outputPath);
+      console.log(`Dosya başarıyla yazıldı: ${outputPath}`);
+    }
+
     res.json({
-      message: 'Dosya başarıyla işlendi ve kaydedildi.',
-      filePath: outputPath,
+      message: "Dosya başarıyla işlendi ve kaydedildi.",
+      filePath: outputPaths,
     });
   } catch (error) {
-    console.error('Bir hata oluştu:', error);
-    res.status(500).json({ error: 'Bir hata oluştu', details: error.message });
+    console.error("Bir hata oluştu:", error);
+    res.status(500).json({ error: "Bir hata oluştu", details: error.message });
   }
 });
 
